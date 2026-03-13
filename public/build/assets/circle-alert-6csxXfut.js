589135d9f58d71948d8c8a4ff8f2e5b1a7a67d46
sangmo-land <sangmolandry12@gmail.com>
2026-03-13 12:02:39 +0100

    feat: add user-plus icon and update manifest with new asset names and imports

---
+++ public/build/assets/circle-alert-6csxXfut.js
@@ -0,0 +1,1 @@
+import{c as e}from"./createLucideIcon-IooRqlp5.js";const c=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],t=e("chevron-right",c);const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],i=e("circle-alert",o);export{i as C,t as a};
